8b0d57f0d2ae46dd68e4db238887e7035d2da398
KAMIKINGDOM\Kami in Black <grabowska.joanna4@gmail.com>
2018-11-17 15:59:14 +0100

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import {TodoItem} from './interfaces/todo-item';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'todo-list';
+  title = 'My To Do List APP';
 }
+
+
+
+
+
